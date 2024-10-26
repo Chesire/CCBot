6dfba77012fc02d6adfb723b4d7ee341cd0c78a6
Troy Rijkaard <troy.rijkaard@epsyhealth.com>
2024-10-26 11:54:18 +0100

    Move server and user commands into info subcommands

--- commands/utility/challenge-add.js
+++ commands/utility/challenge-add.js
@@ -5,12 +5,15 @@ const data = new SlashCommandBuilder()
 		.setDescription('Adds a new challenge for a user')
 	    .addStringOption(option =>
 		    option.setName('input')
-			    .setDescription('The input to echo back'));
+				.setDescription('The input to echo back')
+				.setMaxLength(100)
+				.setRequired(true)
+		);
 
 module.exports = { 
 	cooldown: 5,
 	data: data,   
 	async execute(interaction) {
-		await interaction.reply({ content: 'Some challenges' });
+		await interaction.reply({ content: data.input });
 	}, 
 }; 
---
+++ commands/utility/info.js
@@ -0,0 +1,41 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+const data = new SlashCommandBuilder()
+    .setName("info")
+    .setDescription("Get information about a user or server")
+    .addSubcommand(subCommand =>
+        subCommand
+            .setName("user")
+            .setDescription("Get information about a user")
+            .addUserOption(option =>
+                option
+                    .setName('target')
+                    .setDescription('The user')
+            )
+    )
+    .addSubcommand(subCommand =>
+        subCommand
+            .setName("server")
+            .setDescription("Get information about the server")
+    )
+
+async function handleUserSubcommand(interaction) {
+    const user = interaction.options.getUser('target');
+    await interaction.reply(`${interaction.user.displayName} searched up user\nUsername: ${user.displayName}\nID: ${user.id}`);
+}
+
+async function handleServerSubCommand(interaction) {
+    await interaction.reply(`This server is ${interaction.guild.name} and has ${interaction.guild.memberCount} members.`);
+}
+
+module.exports = {
+	cooldown: 5,
+	data: data,
+    async execute(interaction) {
+        if (interaction.options.getSubcommand() === "user") {
+            handleUserSubcommand(interaction)
+        } else if (interaction.options.getSubcommand() === "server") {
+            handleServerSubCommand(interaction)
+        }
+	},
+};
