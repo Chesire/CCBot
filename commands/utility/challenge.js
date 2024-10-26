bd9a211570012b88c7c7eb17ae2a82e6650214c3
Troy Rijkaard <troy.rijkaard@epsyhealth.com>
2024-10-26 14:02:27 +0100

    Update the challenge

--- commands/utility/challenge.js
+++ commands/utility/challenge.js
@@ -8,17 +8,34 @@ const data = new SlashCommandBuilder()
             .setName("add")
             .setDescription("Adds a challenge for a user")
 	    	.addStringOption(option =>
-			    option.setName('input')
-					.setDescription('The input to echo back')
-					.setMaxLength(100)
+			    option.setName('description')
+					.setDescription('Description of the challenge being done')
+					.setMaxLength(200)
+					.setRequired(true))
+			.addStringOption(option =>
+				option
+					.setName('time-frame')
+					.setDescription('What is the time frame the challenge occurs in?')
 					.setRequired(true)
-			)
-	)
+					.addChoices(
+						{ name: 'Daily', value: 'daily' },
+						{ name: 'Weekly', value: 'weekly' },
+						{ name: 'Monthly', value: 'monthly' }
+				))
+			.addNumberOption(option =>
+				option
+					.setName('cheats')
+					.setDescription('How many cheat days are allowed in each time window?')
+					.setMinValue(0)
+					.setMaxValue(4))
+			.addBooleanOption(option =>
+				option
+					.setName('allow-pause')
+					.setDescription("Are pauses allowed for special occasions?")))
 	.addSubcommand(subCommand =>
         subCommand
             .setName("list-all")
-			.setDescription("Lists all users challenges")
-	)
+			.setDescription("Lists all users challenges"))
 	.addSubcommand(subCommand =>
         subCommand
             .setName("list-user")
@@ -27,17 +44,31 @@ const data = new SlashCommandBuilder()
                 option
                     .setName('target')
                     .setDescription('The user')
-                    .setRequired(true)
-            )
-	)
+                    .setRequired(true)))
 	.addSubcommand(subCommand =>
 		subCommand
 			.setName('remove')
-			.setDescription('Removes a challenge from a user')
-)
+			.setDescription('Removes a challenge from a user'))
+	.addSubcommand(subCommand =>
+		subCommand
+			.setName('cheat')
+			.setDescription('Sets that today is a cheat day'))
+	.addSubcommand(subCommand =>
+		subCommand
+			.setName('pause')
+			.setDescription('Sets a date for a pause day')
+			.addStringOption(option => 
+				option
+					.setName("reason")
+					.setDescription("Why the pause")
+					.setRequired(true)))
 	
 async function addChallenge(interaction) {
-	await interaction.reply(`In add challenge`);
+	const description = interaction.options.getString('description')
+	const timeFrame = interaction.options.getString('time-frame')
+	const cheats = interaction.options.getNumber('cheats')
+	const allowPause = interaction.options.getBoolean('allow-pause')
+	await interaction.reply(`${interaction.user.displayName} is adding a challenge.\nThey will do "${description}" every ${timeFrame}`);
 }
 
 async function listAllChallenges(interaction) {
@@ -64,6 +95,8 @@ module.exports = {
             listMyChallenges(interaction)
         } else if (interaction.options.getSubcommand() === "remove") {
             removeChallenge(interaction)
-        }
-	},
+		} else {
+			await interaction.reply('NYI')
+		}
+	}
 }; 
