246e5e18c50923f0a4b500d0aed3a8b6aeda7e4b
Troy Rijkaard <troy.rijkaard@epsyhealth.com>
2025-11-02 12:37:54 +0000

    Move commands out of utility directory

--- src/commands/admin.js
+++ src/commands/admin.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder, ChannelType } = require('discord.js');
-const adminDb = require('../../database/admindb');
+const adminDb = require('../database/admindb');
 
 const data = new SlashCommandBuilder()
     .setName('admin')
