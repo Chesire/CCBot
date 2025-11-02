382f487316a42e52ca209ec8e4c7816f5afd36e9
Troy Rijkaard <troy.rijkaard@epsyhealth.com>
2025-11-02 13:48:48 +0000

    Extract the command loading logic out
    Extract to a utils folder so we can remove the duplication of code

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, GatewayIntentBits } = require('discord.js');
 const { token } = require('../config.json');
+const { loadCommands } = require('./utils/command-loader');
 
 // Create a new client instance
 const client = new Client({
@@ -14,18 +15,9 @@ const client = new Client({
 });
 client.commands = new Collection();
 
-const commandsPath = path.join(__dirname, 'commands');
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-
-for (const file of commandFiles) {
-	const filePath = path.join(commandsPath, file);
-	const command = require(filePath);
-	// Set a new item in the Collection with the key as the command name and the value as the exported module
-	if ('data' in command && 'execute' in command) {
-		client.commands.set(command.data.name, command);
-	} else {
-		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-	}
+const commands = loadCommands();
+for (const command of commands) {
+	client.commands.set(command.data.name, command);
 }
 
 const eventsPath = path.join(__dirname, 'events');
