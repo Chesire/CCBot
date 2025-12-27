2cf3eacb8dce25ac959c8b52ba77dd9a491af5b2
Troy Rijkaard <troy.rijkaard@chesire.dev>
2025-12-27 18:28:19 +0000

    Remove use of the db in the ready cron file

--- src/core/events/ready-cron.js
+++ src/core/events/ready-cron.js
@@ -1,23 +1,20 @@
 const { Events } = require("discord.js");
 const cron = require("cron");
 const token = require("../../../config.json");
-const admindb = require("../../features/admin/data/admindb");
+const adminRepository = require("../../features/admin/data/admin-repository");
 const challengedb = require("../../features/challenge/data/challengedb");
 
 async function fireDailyCron(client) {
-  console.log("Starting day cron");
-  const db = await admindb.Admin.findOne({ where: { singleid: 0 } });
-  if (!db) {
-    return;
-  }
+  console.log("[ReadyCron] starting day cron");
 
   const guild = await client.guilds.cache.get(token.guildId);
-  const channel = await guild.channels.cache.get(db.challengechannelid);
+  const challengeChannelId = await adminRepository.challengeChannelId.get();
+  const channel = await guild.channels.cache.get(challengeChannelId);
   const challenges = await challengedb.Challenges.findAll({
     where: { timeframe: "daily" },
   });
 
-  console.log(`Found ${challenges.length} challenges`);
+  console.log(`[ReadyCron] found ${challenges.length} challenges`);
   if (challenges.length > 0) {
     const challengesString = challenges
       .map(
@@ -31,19 +28,16 @@ async function fireDailyCron(client) {
 }
 
 async function fireWeeklyCron(client) {
-  console.log("Starting week cron");
-  const db = await admindb.Admin.findOne({ where: { singleid: 0 } });
-  if (!db) {
-    return;
-  }
+  console.log("[ReadyCron] starting week cron");
 
   const guild = await client.guilds.cache.get(token.guildId);
-  const channel = await guild.channels.cache.get(db.challengechannelid);
+  const challengeChannelId = await adminRepository.challengeChannelId.get();
+  const channel = await guild.channels.cache.get(challengeChannelId);
   const challenges = await challengedb.Challenges.findAll({
     where: { timeframe: "weekly" },
   });
 
-  console.log(`Found ${challenges.length} challenges`);
+  console.log(`[ReadyCron] found ${challenges.length} challenges`);
   if (challenges.length > 0) {
     const challengesString = challenges
       .map(
@@ -57,19 +51,16 @@ async function fireWeeklyCron(client) {
 }
 
 async function fireMonthlyCron(client) {
-  console.log("Starting month cron");
-  const db = await admindb.Admin.findOne({ where: { singleid: 0 } });
-  if (!db) {
-    return;
-  }
+  console.log("[ReadyCron] starting month cron");
 
   const guild = await client.guilds.cache.get(token.guildId);
-  const channel = await guild.channels.cache.get(db.challengechannelid);
+  const challengeChannelId = await adminRepository.challengeChannelId.get();
+  const channel = await guild.channels.cache.get(challengeChannelId);
   const challenges = await challengedb.Challenges.findAll({
     where: { timeframe: "monthly" },
   });
 
-  console.log(`Found ${challenges.length} challenges`);
+  console.log(`[ReadyCron] found ${challenges.length} challenges`);
   if (challenges.length > 0) {
     const challengesString = challenges
       .map(
