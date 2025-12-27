8d973f565fa8d035559d2cb15bba5a2cd637f9d9
Troy Rijkaard <troy.rijkaard@chesire.dev>
2025-12-27 11:01:38 +0000

    Call initialize for the admin db

--- src/core/events/ready-db.js
+++ src/core/events/ready-db.js
@@ -1,5 +1,6 @@
 const { Events } = require("discord.js");
 const admindb = require("../../features/admin/data/admindb");
+const adminRepository = require("../../features/admin/data/admin-repository");
 const challengedb = require("../../features/challenge/data/challengedb");
 const shameeventsdb = require("../../features/shame/data/shameeventsdb");
 const wrappeddb = require("../../features/wrapped/data/wrappeddb");
@@ -7,12 +8,20 @@ const wrappeddb = require("../../features/wrapped/data/wrappeddb");
 module.exports = {
   name: Events.ClientReady,
   once: true,
-  execute(client) {
+  async execute(client) {
     console.log(`${client.user.tag} ready. Syncing DBs!`);
 
+    console.log(`Syncing Admin DB`);
     admindb.Admin.sync({ alter: true });
+    await adminRepository.initialize();
+
+    console.log(`Syncing Challenge DB`);
     challengedb.Challenges.sync();
+
+    console.log(`Syncing ShameEvents DB`);
     shameeventsdb.ShameEvents.sync();
+
+    console.log(`Syncing Wrapped DB`);
     wrappeddb.Wrapped.sync();
 
     console.log("DBs Synced!");
