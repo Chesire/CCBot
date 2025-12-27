4e2378aacf0df55d889fe9908017764d7f61da24
Troy Rijkaard <troy.rijkaard@chesire.dev>
2025-12-27 19:25:50 +0000

    Fix to use an IIFE
    Fix as this_ wasn't available during construction otherwise.

--- src/features/admin/data/admin-repository.js
+++ src/features/admin/data/admin-repository.js
@@ -1,48 +1,56 @@
 const { Sequelize } = require("sequelize");
 const admindb = require("./admindb");
 
-const adminRepository = {
-  async initialize() {
-    this._record = await admindb.Admin.findByPk(0);
-    if (!this._record) {
-      console.log("[AdminRepository] no admin record found, creating default");
-      this._record = await admindb.Admin.create({ singleid: 0 });
-    }
-  },
+const adminRepository = (() => {
+  const repo = {
+    _record: null,
 
-  _createSetting(fieldName) {
-    const attributes = admindb.Admin.getAttributes();
-    const fieldType = attributes[fieldName]?.type;
-    const defaultValue = attributes[fieldName]?.defaultValue;
-    const self = this;
-
-    const coerce = (value) => {
-      // For now only Boolean and String types are used
-      if (fieldType instanceof admindb.Sequelize.BOOLEAN) {
-        return Boolean(value);
+    async initialize() {
+      this._record = await admindb.Admin.findByPk(0);
+      if (!this._record) {
+        console.log(
+          "[AdminRepository] no admin record found, creating default",
+        );
+        this._record = await admindb.Admin.create({ singleid: 0 });
       }
-      return String(value);
-    };
+    },
 
-    return {
-      async get() {
-        return self._record?.[fieldName] ?? defaultValue;
-      },
-      async set(value) {
-        if (!self._record) {
-          self._record = await admindb.Admin.findByPk(0);
+    _createSetting(fieldName) {
+      const attributes = admindb.Admin.getAttributes();
+      const fieldType = attributes[fieldName]?.type;
+      const defaultValue = attributes[fieldName]?.defaultValue;
+      const self = this;
+
+      const coerce = (value) => {
+        // For now only Boolean and String types are used
+        if (fieldType instanceof Sequelize.BOOLEAN) {
+          return Boolean(value);
         }
-        await self._record.update({ [fieldName]: coerce(value) });
-      },
-      isDefault(value) {
-        return coerce(value).toString() === defaultValue.toString();
-      },
-    };
-  },
+        return String(value);
+      };
 
-  allowBotShameReplies: this._createSetting("allowbotshamereplies"),
-  challengeChannelId: this._createSetting("challengechannelid"),
-  shamedRoleId: this._createSetting("shamedroleid"),
-};
+      return {
+        async get() {
+          return self._record?.[fieldName] ?? defaultValue;
+        },
+        async set(value) {
+          if (!self._record) {
+            self._record = await admindb.Admin.findByPk(0);
+          }
+          await self._record.update({ [fieldName]: coerce(value) });
+        },
+        isDefault(value) {
+          return coerce(value).toString() === defaultValue.toString();
+        },
+      };
+    },
+  };
+
+  repo.allowBotShameReplies = repo._createSetting("allowbotshamereplies");
+  repo.challengeChannelId = repo._createSetting("challengechannelid");
+  repo.shamedRoleId = repo._createSetting("shamedroleid");
+
+  return repo;
+})();
 
 module.exports = adminRepository;
