2e3e973525d4c06aca69b02e4505654ea223cb1c
Troy Rijkaard <troy.rijkaard@chesire.dev>
2025-12-28 22:00:37 +0000

    Sync the event db

--- src/core/events/ready-db.js
+++ src/core/events/ready-db.js
@@ -2,6 +2,7 @@ const { Events } = require("discord.js");
 const admindb = require("../../features/admin/data/admindb");
 const adminRepository = require("../../features/admin/data/admin-repository");
 const challengedb = require("../../features/challenge/data/challengedb");
+const eventdb = require("../../core/data/eventdb");
 const shameeventsdb = require("../../features/shame/data/shameeventsdb");
 const wrappeddb = require("../../features/wrapped/data/wrappeddb");
 
@@ -23,6 +24,10 @@ module.exports = {
 
     console.log(`[ReadyDB] syncing Wrapped db`);
     await wrappeddb.Wrapped.sync();
+
+    console.log(`[ReadyDB] syncing Event db`);
+    await eventdb.UserYearEvent.sync({ alter: true });
+    await eventdb.ChannelYearEvent.sync({ alter: true });
 
     console.log("[ReadyDB] dbs Synced!");
   },
