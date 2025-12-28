c246e040fe444f433c11c564aacc0dfd1489728e
Troy Rijkaard <troy.rijkaard@chesire.dev>
2025-12-28 21:56:00 +0000

    Add new database file for event tracking

---
+++ src/core/data/eventdb.js
@@ -0,0 +1,32 @@
+const { Sequelize } = require("sequelize");
+const path = require("path");
+
+const sequelize = new Sequelize({
+  dialect: "sqlite",
+  logging: false,
+  storage: path.join(__dirname, "../../../../data/eventdb.sqlite"),
+});
+
+const UserYearEvent = sequelize.define("useryearevent", {
+  userid: Sequelize.TEXT,
+  year: Sequelize.INTEGER,
+  eventtype: Sequelize.TEXT,
+  count: {
+    type: Sequelize.INTEGER,
+    defaultValue: 0,
+  },
+});
+UserYearEvent.addIndex(["userid", "year", "eventtype"], { unique: true });
+
+const ChannelYearEvent = sequelize.define("channelyearevent", {
+  channelid: Sequelize.TEXT,
+  year: Sequelize.INTEGER,
+  eventtype: Sequelize.TEXT,
+  count: {
+    type: Sequelize.INTEGER,
+    defaultValue: 0,
+  },
+});
+ChannelYearEvent.addIndex(["channelid", "year", "eventtype"], { unique: true });
+
+module.exports = { UserYearEvent, ChannelYearEvent };
