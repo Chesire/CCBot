005ba3241f69b3ff3995c8e08f9fd5ab7b27909c
Troy Rijkaard <troy.rijkaard@chesire.dev>
2025-12-29 09:41:07 +0000

    Remove the old wrappeddb

--- src/core/events/ready-db.js
+++ src/core/events/ready-db.js
@@ -4,7 +4,6 @@ const adminRepository = require("../../features/admin/data/admin-repository");
 const challengedb = require("../../features/challenge/data/challengedb");
 const eventdb = require("../../core/data/eventdb");
 const shameeventsdb = require("../../features/shame/data/shameeventsdb");
-const wrappeddb = require("../../features/wrapped/data/wrappeddb");
 
 module.exports = {
   name: Events.ClientReady,
@@ -21,9 +20,6 @@ module.exports = {
 
     console.log(`[ReadyDB] syncing ShameEvents db`);
     await shameeventsdb.ShameEvents.sync();
-
-    console.log(`[ReadyDB] syncing Wrapped db`);
-    await wrappeddb.Wrapped.sync();
 
     console.log(`[ReadyDB] syncing Event db`);
     await eventdb.UserYearEvent.sync({ alter: true });
