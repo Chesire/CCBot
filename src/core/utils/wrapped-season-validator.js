893689f4dd631b49c06b9538d24518f1b752a0ce
Troy Rijkaard <troy.rijkaard@chesire.dev>
2025-12-29 09:39:16 +0000

    Disable the wrapped command for now
    Disable for now until we migrate to the new DB correctly in wrapped, this can be done later in the year once we know what events will be tracked fully.

--- src/core/utils/wrapped-season-validator.js
+++ src/core/utils/wrapped-season-validator.js
@@ -3,12 +3,14 @@
  * @returns {boolean} true if within wrapped season
  */
 function isWrappedSeason() {
+  // For now just return false, we want to update it
+  return false;
   const now = new Date();
   const month = now.getMonth(); // 0 based
   const day = now.getDate();
 
-  // from just before xmas -> end of Jan
-  return (month === 11 && day >= 23) || month === 0;
+  // from Christmas day -> end of Jan
+  return (month === 11 && day >= 25) || month === 0;
 }
 
 module.exports = { isWrappedSeason };
