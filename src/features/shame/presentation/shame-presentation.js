a7bef0ad4f5fee007cca59a7de489f23d9c64100
Troy Rijkaard <troy.rijkaard@chesire.dev>
2025-12-30 21:54:46 +0000

    Add some new shame gifs

--- src/features/shame/presentation/shame-presentation.js
+++ src/features/shame/presentation/shame-presentation.js
@@ -4,6 +4,10 @@ const _shameGifs = [
   "https://tenor.com/bSiK8.gif",
   "https://tenor.com/bLQnA.gif",
   "https://tenor.com/uDmFdQcabLN.gif",
+  "https://tenor.com/view/shame-max-mitchell-wild-cards-shame-on-you-how-dare-you-gif-1059570400115177835",
+  "https://tenor.com/view/embarrassed-embarassed-benjammins-ben-jammins-shame-gif-15838104130396461000",
+  "https://tenor.com/view/crypto-cardano-nikepig-mood-reaction-gif-5532924884402701643",
+  "https://tenor.com/view/shame-shameless-ashamed-shame-on-you-shameful-gif-3258148063038839630",
 ];
 
 const shamePresentation = {
